fb8a165320e5928adcb300757b005e8faa7eb1bc
Mike Dunn <mike@eikonomega.com>
2019-08-12 16:58:40 -0400

    Adjust linear gradients for header background.

--- src/components/HeaderBackground.js
+++ src/components/HeaderBackground.js
@@ -8,7 +8,7 @@ export default () => {
         gridColumn: '1/4',
         gridRow: '1/1',
         background:
-          'linear-gradient(180deg, rgba(255, 255, 255,0) 0%, rgba(255, 255, 255,1) 100%), linear-gradient(90deg, rgba(229, 161, 83,1) 0%, rgba(74, 116, 168, 1) 100%)',
+          'linear-gradient(180deg, rgba(255, 255, 255,0) 0%, rgba(255, 255, 255,1) 100%), linear-gradient(90deg, rgba(229, 123, 0,1) 0%, rgba(0, 76, 168, 1) 100%)',
         zIndex: -10
       }}
     />
