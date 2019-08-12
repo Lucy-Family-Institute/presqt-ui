5f2db4f104b2bb4a7af15be3cf68ee0ca5bf10d1
Mike Dunn <mike@eikonomega.com>
2019-08-12 16:13:01 -0400

    Start adding content to the global nav.

--- src/components/HeaderMenu.js
+++ src/components/HeaderMenu.js
@@ -1,21 +1,23 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import PropTypes from 'prop-types';
 
-const HeaderMenu = props => {
+import textStyles from '../styles/text';
+
+const HeaderMenu = () => {
   return (
     <div
       css={{
         gridArea: 'headerMenu',
-        background:
-          'linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(221,221,221,1) 100%)'
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        paddingRight: 50
       }}
     >
-      Header Menu
+      <span className={textStyles.globalNav}>About PresQT</span>
     </div>
   );
 };
-
-HeaderMenu.propTypes = {};
 
 export default HeaderMenu;
