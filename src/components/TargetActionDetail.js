fbfd926baddf80e5201de9b6b4fe30e6777d7aa2
Mike Dunn <mike@eikonomega.com>
2019-09-11 12:28:13 -0400

    Add some actual functionality to `TargetActionDetail` component.

--- src/components/TargetActionDetail.js
+++ src/components/TargetActionDetail.js
@@ -1,18 +1,63 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 
 import textStyles from '../styles/text';
+import MediumHeader from './widgets/MediumHeader';
 
-const TargetActionDetail = props => {
+export default function TargetActionDetail() {
+  const selectedSourceResource = useSelector(
+    state => state.resources.selectedInSource
+  );
+
+  /**
+   * Filter out resource data points that we don't want to
+   * display in the detail panel.
+   */
+  const detailsToRender = resource => {
+    return Object.entries(resource)
+      .filter(resourceDetail => {
+        const [key, value] = resourceDetail;
+        const isKeyWanted = ['links', 'open', 'children', 'count'].includes(key)
+          ? false
+          : true;
+        const isValueNotNull = value !== null;
+
+        console.log(key, isKeyWanted, isValueNotNull);
+
+        return isKeyWanted && isValueNotNull;
+      })
+      .map(resourceDetail => {
+        const [key, value] = resourceDetail;
+        return [
+          key
+            .split('_')
+            .map(element => element[0].toUpperCase() + element.slice(1))
+            .join(' '),
+          value
+        ];
+      });
+  };
+
+  const renderDetailItem = item => {
+    const [key, value] = item;
+    return (
+      <div
+        css={{ display: 'flex', flexDirection: 'column', paddingBottom: 10 }}
+      >
+        <span css={[textStyles.body, { fontWeight: 500 }]}>{key}</span>
+        {['string', 'number'].includes(typeof value) ? (
+          <span css={[textStyles.body, { fontSize: 12 }]}>{value}</span>
+        ) : null}
+      </div>
+    );
+  };
+
   return (
     <div
       css={[
         css({
           gridArea: 'targetActionDetail',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
           borderLeftColor: '#C5C5C5',
           borderLeftWidth: 1,
           borderLeftStyle: 'solid',
@@ -20,11 +65,18 @@ const TargetActionDetail = props => {
         })
       ]}
     >
-      Target Resource Detail Will Go Here
+      {selectedSourceResource ? (
+        <div>
+          <MediumHeader text='Resource Details' />
+          <div css={{ paddingTop: 10 }}>
+            {detailsToRender(selectedSourceResource).map(resourceData =>
+              renderDetailItem(resourceData)
+            )}
+          </div>
+        </div>
+      ) : (
+        `Target Resource Detail Will Go Here`
+      )}
     </div>
   );
-};
-
-TargetActionDetail.propTypes = {};
-
-export default TargetActionDetail;
+}
