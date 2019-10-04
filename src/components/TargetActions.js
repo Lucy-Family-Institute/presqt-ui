792bc723f802ed6759ddae43bc7c8a0382d02c46
Mike Dunn <mike@eikonomega.com>
2019-10-04 09:52:27 -0400

    Some WIP stuff for downloading resources.

--- src/components/TargetActions.js
+++ src/components/TargetActions.js
@@ -1,14 +1,45 @@
 /** @jsx jsx */
+import { useState, useEffect } from 'react';
 import { jsx } from '@emotion/core';
 import { useSelector } from 'react-redux';
 
 import textStyles from '../styles/text';
 import ActionButton from './widgets/ActionButton';
+import { initiateResourceDownload } from '../api/resources';
+import useModal from '../hooks/useModal';
+import DownloadModal from './modals/DownloadModal';
+
+const temporaryLinktoFunctionMap = {
+  Download: initiateResourceDownload
+};
+
+const actionLinkToComponent = {
+  Download: DownloadModal
+};
 
 export default function TargetActions() {
-  const selectedSourceResource = useSelector(
-    state => state.resources.selectedInSource
-  );
+  const [
+    selectedSourceResource,
+    sourceTargetToken,
+    pendingAPIOperations
+  ] = useSelector(state => [
+    state.resources.selectedInSource,
+    state.targets.source
+      ? state.authorization.apiTokens[state.targets.source.name]
+      : null,
+    state.resources.pendingAPIOperations
+  ]);
+
+  const { modalVisible, toggleModalVisibility } = useModal();
+  const [modalType, setModalType] = useState(null);
+  const [link, setLink] = useState(null);
+
+  useEffect(() => {
+    if (link) {
+      setModalType(() => actionLinkToComponent[link.name]);
+      toggleModalVisibility();
+    }
+  }, [link, toggleModalVisibility]);
 
   return (
     <div
@@ -33,10 +64,20 @@ export default function TargetActions() {
       >
         {selectedSourceResource ? selectedSourceResource.title : null}
       </span>
+
+      <DownloadModal
+        modalActive={modalVisible}
+        onHide={toggleModalVisibility}
+      />
+
       <div css={{ display: 'flex', flexDirection: 'row', marginTop: 10 }}>
         {selectedSourceResource &&
           selectedSourceResource.links.map(link => (
-            <ActionButton key={link.name} text={link.name} />
+            <ActionButton
+              key={link.name}
+              text={link.name}
+              onClick={() => setLink(link)}
+            />
           ))}
       </div>
     </div>
