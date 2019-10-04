5ca44e32ec725471d2b1206bb99363d6e914933a
Mike Dunn <mike@eikonomega.com>
2019-10-04 09:49:51 -0400

    Add WIP api interactions for file download.

--- src/api/resources.js
+++ src/api/resources.js
@@ -1,9 +1,41 @@
 import axios from 'axios';
+import FileSaver from 'file-saver';
 
 import { apiURLBase } from '../config';
 
-export function getSourceTargetResources(sourceTarget, sourceTargetToken) {
+export function getTargetResources(sourceTarget, sourceTargetToken) {
   return axios.get(`${apiURLBase}targets/${sourceTarget}/resources/`, {
     headers: { 'presqt-source-token': sourceTargetToken }
   });
+}
+
+export function getResourceDetail(resource, sourceTargetToken) {
+  const resourceDetailURL = resource.links.find(link => link.name === 'Detail')
+    .link;
+  return axios.get(resourceDetailURL, {
+    headers: { 'presqt-source-token': sourceTargetToken }
+  });
+}
+
+export function initiateResourceDownload(
+  resourceDownloadURL,
+  sourceTargetToken
+) {
+  const closure = url => {
+    axios
+      .get(url, {
+        headers: { 'presqt-source-token': sourceTargetToken },
+        responseType: 'blob',
+        timeout: 10000
+      })
+      .then(response => FileSaver(response.data, 'textname.txt'));
+  };
+
+  axios
+    .get(resourceDownloadURL, {
+      headers: { 'presqt-source-token': sourceTargetToken }
+    })
+    .then(response => {
+      setInterval(closure, 5000, response.data.download_job);
+    });
 }
