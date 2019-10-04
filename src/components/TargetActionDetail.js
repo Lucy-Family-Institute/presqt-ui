0ac786961a633f8ea6e9e43fdea5545059efc549
Mike Dunn <mike@eikonomega.com>
2019-10-04 09:50:46 -0400

    Add ability to display scalar and nested data attributes of resources in `TargetActionDetail`

--- src/components/TargetActionDetail.js
+++ src/components/TargetActionDetail.js
@@ -13,6 +13,8 @@ export default function TargetActionDetail() {
   /**
    * Filter out resource data points that we don't want to
    * display in the detail panel.
+   *
+   * We exclude the following keys: ['links', 'open', 'children', 'count']
    */
   const detailsToRender = resource => {
     return Object.entries(resource)
@@ -21,11 +23,8 @@ export default function TargetActionDetail() {
         const isKeyWanted = ['links', 'open', 'children', 'count'].includes(key)
           ? false
           : true;
-        const isValueNotNull = value !== null;
 
-        console.log(key, isKeyWanted, isValueNotNull);
-
-        return isKeyWanted && isValueNotNull;
+        return isKeyWanted;
       })
       .map(resourceDetail => {
         const [key, value] = resourceDetail;
@@ -41,17 +40,51 @@ export default function TargetActionDetail() {
 
   const renderDetailItem = item => {
     const [key, value] = item;
+    let renderer;
+    if (['string', 'number'].includes(typeof value)) {
+      renderer = renderScalarItem;
+    } else if (['object'].includes(typeof value)) {
+      value !== null ? (renderer = renderObject) : (renderer = renderNull);
+    }
+
     return (
       <div
         css={{ display: 'flex', flexDirection: 'column', paddingBottom: 10 }}
+        key={key}
       >
         <span css={[textStyles.body, { fontWeight: 500 }]}>{key}</span>
-        {['string', 'number'].includes(typeof value) ? (
-          <span css={[textStyles.body, { fontSize: 12 }]}>{value}</span>
-        ) : null}
+        {renderer(value)}
       </div>
     );
   };
+
+  const renderScalarItem = value => (
+    <span css={[textStyles.body, { fontSize: 12 }]}>{value}</span>
+  );
+
+  const renderObject = value => {
+    return Object.entries(value).map(resourceDetailElement => {
+      const [key, value] = resourceDetailElement;
+      console.log(key, value);
+      return (
+        <div css={{ display: 'flex', flexDirection: 'row' }}>
+          <span
+            css={[
+              textStyles.body,
+              { fontSize: 12, paddingLeft: 5, fontWeight: 500 }
+            ]}
+          >
+            {`${key}:\u00a0`}
+          </span>
+          <span css={[textStyles.body, { fontSize: 12 }]}>{`${value}`}</span>
+        </div>
+      );
+    });
+  };
+
+  const renderNull = () => (
+    <span css={[textStyles.body, { fontSize: 12 }]}>null</span>
+  );
 
   return (
     <div
@@ -74,9 +107,7 @@ export default function TargetActionDetail() {
             )}
           </div>
         </div>
-      ) : (
-        `Target Resource Detail Will Go Here`
-      )}
+      ) : null}
     </div>
   );
 }
