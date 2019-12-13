c568d8a6c7af305fad4c4926b990fd93a56f3430
Justin Branco <jbranco@nd.edu>
2019-12-13 14:36:29 -0500

    Search is broken

--- src/components/TargetSearch.js
+++ src/components/TargetSearch.js
@@ -1,13 +1,24 @@
 /** @jsx jsx */
-import {jsx} from '@emotion/core';
-import { useState } from 'react';
-import {actionCreators} from "../redux/actionCreators";
-import {useDispatch, useSelector} from "react-redux";
-import {formatSearch} from "../redux/utils";
-import { SearchInput } from '@bit/segmentio.evergreen.search-input';
-import textStyles from "../styles/text";
+import { jsx } from "@emotion/core";
+import { useState } from "react";
+import { actionCreators } from "../redux/actionCreators";
+import { useDispatch, useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      marginTop: 10,
+      width: 250
+    }
+  }
+}));
 
 export default function TargetSearch() {
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   /** SELECTOR DEFINITIONS
@@ -20,30 +31,69 @@ export default function TargetSearch() {
   /** STATE DEFINITIONS
    * [searchValue, updateSearch] : Search value state
    **/
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
 
   const token = apiTokens[sourceTarget.name];
 
   const submitSearch = () => {
-    dispatch(actionCreators.resources.removeFromErrorList(
-      actionCreators.resources.loadFromSourceTargetSearch.toString())
+    dispatch(
+      actionCreators.resources.removeFromErrorList(
+        actionCreators.resources.loadFromSourceTargetSearch.toString()
+      )
     );
-    dispatch(actionCreators.resources.loadFromSourceTargetSearch(
-      sourceTarget.name, token, searchValue)
+    dispatch(
+      actionCreators.resources.loadFromSourceTargetSearch(
+        sourceTarget.name,
+        token,
+        searchValue
+      )
     );
   };
 
+  const CssTextField = withStyles({
+    root: {
+      "& label.Mui-focused": {
+        color: "#0C52A7"
+      },
+      "& .MuiInput-underline:after": {
+        borderBottomColor: "#0C52A7"
+      },
+      "& .MuiOutlinedInput-root": {
+        "&.Mui-focused fieldset": {
+          borderColor: "#0C52A7"
+        }
+      }
+    }
+  })(TextField);
+
   return (
-    <div css={{marginBottom: 10}}>
-      <SearchInput
+    <div css={{ marginBottom: 10 }}>
+      <form className={classes.root} noValidate autoComplete="off">
+        <CssTextField
+          color="success"
+          size="small"
+          type="text"
+          id="outlined-basic"
+          label={"Search " + sourceTarget.readable_name}
+          variant="outlined"
+          value={searchValue}
+          onChange={event => setSearchValue(event.target.value)}
+        />
+      </form>
+      <Button
+        variant="contained"
+        color="secondary"
+        type="submit"
+        onClick={submitSearch}
+      >
+        Search
+      </Button>
+      {/* <TextField
         type="text"
         placeholder={"Search all " + sourceTarget.readable_name + " resources"}
         value={searchValue}
         onChange={event => setSearchValue(event.target.value)}
-      />
-      <button type="submit" onClick={submitSearch}>
-        Search
-      </button>
+      />*/}
     </div>
-  )
-};
+  );
+}
