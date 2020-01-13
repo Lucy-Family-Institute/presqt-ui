35f839caf4fd812eb167e7b4276af8cb5369730b
bfox6 <bfox6@nd.edu>
2020-01-13 12:22:49 -0500

    Updated upload results step to use grid

--- src/components/widgets/UploadStepper.js
+++ src/components/widgets/UploadStepper.js
@@ -11,9 +11,16 @@ import SelectFile from "./SelectFile";
 import Button from "@material-ui/core/Button";
 import DuplicateActionRadioButtons from "./DuplicateActionRadioButtons";
 import UploadButton from "./UploadButton";
-import Spinner from "./Spinner";
 import {useSelector} from "react-redux";
 import LeftSpinner from "./LeftSpinner";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -113,25 +120,67 @@ export default function UploadStepper() {
   useEffect(() => {
     if (sourceUploadStatus === 'success') {
       const failedFixityMessage = sourceUploadData.failed_fixity.length > 0
-          ? `The following files failed fixity: ${sourceUploadData.failed_fixity.join(', ')}`
-          : 'No files failed fixity.';
+          ? [
+            `The following files failed fixity: ${sourceUploadData.failed_fixity.join(', ')}`,
+            <ErrorOutlineIcon color="error"/>
+          ]
+          : [
+            'No files failed fixity',
+            <CheckCircleOutlineIcon color="primary"/>
+          ];
       const resourcesIgnoredMessage = sourceUploadData.resources_ignored.length > 0
-        ? `The following duplicate resources were ignored:
-          ${sourceUploadData.resources_ignored.join(', ')}`
-        : 'No duplicate resources were ignored.';
+        ? [
+          `The following duplicate resources were ignored: 
+          ${sourceUploadData.resources_ignored.join(', ')}`,
+          <ErrorOutlineIcon color="error"/>
+        ]
+        : [
+          'No duplicate resources were ignored',
+          <CheckCircleOutlineIcon color="primary"/>
+        ];
       const resourcesUpdatedMessage = sourceUploadData.resources_updated.length > 0
-        ? `The following duplicate resources were updated:
-          ${sourceUploadData.resources_updated.join(', ')}`
-        : 'No duplicate resources were updated.';
+        ? [
+          `The following duplicate resources were updated:
+          ${sourceUploadData.resources_updated.join(', ')}`,
+          <ErrorOutlineIcon color="error"/>
+        ]
+        : [
+          'No duplicate resources were updated',
+          <CheckCircleOutlineIcon color="primary"/>
+        ];
 
-      const successfulMessage = <div>
-        <br/>
-        {failedFixityMessage}
-        <br />
-        {resourcesIgnoredMessage}
-        <br />
-        {resourcesUpdatedMessage}
-      </div>;
+      const successfulMessage =
+        <Grid item md={6}>
+          <div>
+            <List dense={true}>
+              <ListItem>
+                <ListItemIcon>
+                  {failedFixityMessage[1]}
+                </ListItemIcon>
+                <ListItemText
+                  primary={failedFixityMessage[0]}
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  {resourcesIgnoredMessage[1]}
+                </ListItemIcon>
+                <ListItemText
+                  primary={resourcesIgnoredMessage[0]}
+                />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  {resourcesUpdatedMessage[1]}
+                </ListItemIcon>
+                <ListItemText
+                  primary={resourcesUpdatedMessage[0]}
+                />
+              </ListItem>
+            </List>
+          </div>
+        </Grid>;
+
       setStepThreeContent(successfulMessage);
     }
     else if (sourceUploadStatus === 'failure') {
