692d13224adb84173b0262e0ec499b4b740f7986
bfox6 <bfox6@nd.edu>
2020-01-13 10:47:41 -0500

    Separated modal styles to shared file

--- src/components/modals/DownloadModal.js
+++ src/components/modals/DownloadModal.js
@@ -1,84 +1,16 @@
 /** @jsx jsx */
 import {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
-import { jsx, css } from '@emotion/core';
+import { jsx } from '@emotion/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
-
+import modalStyles from "../../styles/modal";
 import textStyles from '../../styles/text';
-import { basicFadeIn, basicFadeOut } from '../../styles/animations';
 import useAnimatedState from '../../hooks/useAnimatedState';
 import { useDispatch, useSelector } from 'react-redux';
 import FileSaver from 'file-saver';
 import { actionCreators } from '../../redux/actionCreators';
 import Spinner from '../widgets/Spinner';
-
-const styles = {
-  darkenBackground: css({
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    zIndex: 1040,
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#000',
-    opacity: 0.5
-  }),
-  modalContainer: css({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'fixed',
-    padding: '.5rem',
-    top: 0,
-    left: 0,
-    zIndex: 1050,
-    width: '100%',
-    height: '100%'
-  }),
-  modal: css({
-    zIndex: 100,
-    background: 'white',
-    position: 'relative',
-    borderRadius: 10,
-    display: 'flex',
-    flexDirection: 'column',
-    width: '50%'
-  }),
-  modalHeader: css({
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexBasis: 50,
-    padding: 20,
-    backgroundColor: 'rgba(229, 123, 0,1)',
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5
-  }),
-  button: css({
-    backgroundColor: 'rgba(0, 76, 168, 1)',
-    color: 'white',
-    border: 'none',
-    borderRadius: 5
-  }),
-  disabledButton: css({
-    backgroundColor: 'rgba(108, 133, 163, 1)',
-    color: 'white',
-    border: 'none',
-    borderRadius: 5
-  }),
-  fadeIn: css({
-    animationFillMode: 'forwards',
-    animationName: `${basicFadeIn}`,
-    animationDuration: '.4s'
-  }),
-  fadeOut: css({
-    animationFillMode: 'forwards',
-    animationName: `${basicFadeOut}`,
-    animationDuration: '.4s'
-  })
-};
 
 /**
  * This component handles the download modal.
@@ -152,15 +84,15 @@ export default function DownloadModal({ modalActive, toggleModal }) {
           }
         >
           <div
-            css={state.desiredVisibility ? styles.fadeIn : styles.fadeOut}
+            css={state.desiredVisibility ? modalStyles.fadeIn : modalStyles.fadeOut}
             onAnimationEnd={() => {
               state.endAnimationCallback();
             }}
           >
-            <div css={styles.darkenBackground} />
-            <div css={styles.modalContainer} aria-modal aria-hidden>
-              <div css={styles.modal}>
-                <div css={styles.modalHeader}>
+            <div css={modalStyles.darkenBackground} />
+            <div css={modalStyles.modalContainer} aria-modal aria-hidden>
+              <div css={modalStyles.modal}>
+                <div css={modalStyles.modalHeader}>
                   <span css={textStyles.modalTitle}>
                     {sourceDownloadStatus === 'failure'
                       ? 'Download Failed!'
--- src/components/modals/UploadModal.js
+++ src/components/modals/UploadModal.js
@@ -1,82 +1,15 @@
 /** @jsx jsx */
 import ReactDOM from "react-dom";
 import textStyles from "../../styles/text";
+import modalStyles from "../../styles/modal";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faWindowClose} from "@fortawesome/free-solid-svg-icons";
-import {css, jsx} from "@emotion/core";
+import {jsx} from "@emotion/core";
 import useAnimatedState from "../../hooks/useAnimatedState";
-import {basicFadeIn, basicFadeOut} from "../../styles/animations";
 import {useEffect, useState} from "react";
 import UploadInput from "../widgets/UploadInput";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {actionCreators} from "../../redux/actionCreators";
-
-const styles = {
-  darkenBackground: css({
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    zIndex: 1040,
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#000',
-    opacity: 0.5
-  }),
-  modalContainer: css({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'fixed',
-    padding: '.5rem',
-    top: 0,
-    left: 0,
-    zIndex: 1050,
-    width: '100%',
-    height: '100%'
-  }),
-  modal: css({
-    zIndex: 100,
-    background: 'white',
-    position: 'relative',
-    borderRadius: 10,
-    display: 'flex',
-    flexDirection: 'column',
-    width: '50%'
-  }),
-  modalHeader: css({
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexBasis: 50,
-    padding: 20,
-    backgroundColor: 'rgba(229, 123, 0,1)',
-    borderTopLeftRadius: 5,
-    borderTopRightRadius: 5
-  }),
-  button: css({
-    backgroundColor: 'rgba(0, 76, 168, 1)',
-    color: 'white',
-    border: 'none',
-    borderRadius: 5
-  }),
-  disabledButton: css({
-    backgroundColor: 'rgba(108, 133, 163, 1)',
-    color: 'white',
-    border: 'none',
-    borderRadius: 5
-  }),
-  fadeIn: css({
-    animationFillMode: 'forwards',
-    animationName: `${basicFadeIn}`,
-    animationDuration: '.4s'
-  }),
-  fadeOut: css({
-    animationFillMode: 'forwards',
-    animationName: `${basicFadeOut}`,
-    animationDuration: '.4s'
-  })
-};
 
 export default function UploadModal({ modalActive, toggleModal }) {
   const dispatch = useDispatch();
@@ -107,15 +40,15 @@ export default function UploadModal({ modalActive, toggleModal }) {
         }
       >
         <div
-          css={state.desiredVisibility ? styles.fadeIn : styles.fadeOut}
+          css={state.desiredVisibility ? modalStyles.fadeIn : modalStyles.fadeOut}
           onAnimationEnd={() => {
             state.endAnimationCallback();
           }}
         >
-          <div css={styles.darkenBackground} />
-          <div css={styles.modalContainer} aria-modal aria-hidden>
-            <div css={styles.modal}>
-              <div css={styles.modalHeader}>
+          <div css={modalStyles.darkenBackground} />
+          <div css={modalStyles.modalContainer} aria-modal aria-hidden>
+            <div css={modalStyles.modal}>
+              <div css={modalStyles.modalHeader}>
                   <span css={textStyles.modalTitle}>
                     {modalHeader}
                   </span>
