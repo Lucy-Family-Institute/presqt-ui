eab0f2a6269d90be0ef0503023c6da5e04d32c87
Justin Branco <jbranco@nd.edu>
2020-01-16 19:08:45 -0500

    DownloadModal work

--- src/components/modals/DownloadModal.js
+++ src/components/modals/DownloadModal.js
@@ -1,12 +1,13 @@
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "./modalHeader";
 import textStyles from "../../styles/text";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import DialogContent from "@material-ui/core/DialogContent";
 import Spinner from "../widgets/Spinner";
 import FileSaver from "file-saver";
-import {actionCreators} from "../../redux/actionCreators";
+import { actionCreators } from "../../redux/actionCreators";
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 
 export default function DownloadModal({ modalState, setModalState }) {
   const dispatch = useDispatch();
@@ -16,11 +17,18 @@ export default function DownloadModal({ modalState, setModalState }) {
    * sourceDownloadStatus   : String status of the status of the download/
    *                          [null, 'pending', 'successful', 'failure']
    **/
-  const sourceDownloadContents = useSelector(state => state.resources.sourceDownloadContents);
-  const sourceDownloadStatus = useSelector(state => state.resources.sourceDownloadStatus);
+  const sourceDownloadContents = useSelector(
+    state => state.resources.sourceDownloadContents
+  );
+  const sourceDownloadStatus = useSelector(
+    state => state.resources.sourceDownloadStatus
+  );
 
-  const [modalMessage, setModalMessage] = useState('' +
-    'The download is being processed on the server. Please do not leave the page.');
+  const [modalMessage, setModalMessage] = useState(
+    "" +
+      "The download is being processed on the server. Please do not leave the page."
+  );
+  const [egg, setEgg] = useState(`${textStyles.cubsRed}`);
 
   /**
    * Watch for the sourceDownloadStatus to change to 'failure' or 'success'.
@@ -29,16 +37,19 @@ export default function DownloadModal({ modalState, setModalState }) {
    **/
   useEffect(() => {
     // Download failed
-    if (sourceDownloadStatus === 'failure') {
+    if (sourceDownloadStatus === "failure") {
+      setEgg(textStyles.cubsRed);
+      console.log(egg);
       setModalMessage(
-        'Download returned a ' +
-        sourceDownloadContents.status_code +
-        ' status code. ' +
-        sourceDownloadContents.message)
+        "Download returned a " +
+          sourceDownloadContents.status_code +
+          " status code. " +
+          sourceDownloadContents.message
+      );
     }
     // Download successful
-    else if (sourceDownloadStatus === 'success') {
-      FileSaver.saveAs(sourceDownloadContents, 'PresQT_Download.zip');
+    else if (sourceDownloadStatus === "success") {
+      FileSaver.saveAs(sourceDownloadContents, "PresQT_Download.zip");
       setModalState(false);
     }
   }, [sourceDownloadStatus]);
@@ -49,27 +60,36 @@ export default function DownloadModal({ modalState, setModalState }) {
    **/
   const handleClose = () => {
     setModalState(false);
-    setModalMessage('The download is being processed on the server. Please do not leave the page.');
+    setModalMessage(
+      "The download is being processed on the server. Please do not leave the page."
+    );
     dispatch(actionCreators.resources.clearDownloadData());
-    dispatch(actionCreators.resources.removeFromErrorList(
-      actionCreators.resources.downloadResource.toString()));
+    dispatch(
+      actionCreators.resources.removeFromErrorList(
+        actionCreators.resources.downloadResource.toString()
+      )
+    );
   };
 
-  return modalState
-    ? <div>
-      <Dialog maxWidth="md" fullWidth={true} open={modalState} onClose={handleClose} aria-labelledby={"form-dialog-title"}>
+  return modalState ? (
+    <div css={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+      <Dialog
+        maxWidth="md"
+        fullWidth={true}
+        open={modalState}
+        onClose={handleClose}
+        aria-labelledby={"form-dialog-title"}
+      >
         <DialogTitle id="form-dialog-title" onClose={handleClose}>
-          {sourceDownloadStatus === 'failure'
-            ? 'Download Failed!'
-            : 'Download In Progress'}
+          {sourceDownloadStatus === "failure"
+            ? "Download Failed!"
+            : "Download In Progress"}
         </DialogTitle>
-        <DialogContent repositionOnUpdate={false} style={{padding:20}}>
-          <div
-          >
-            <p css={textStyles.body}>
-              {modalMessage}
-            </p>
-            {sourceDownloadStatus === 'pending' ||
+        <DialogContent repositionOnUpdate={false} style={{ padding: 20 }}>
+          <div>
+            {sourceDownloadStatus === 'failure' ? <ErrorOutlineIcon color="error"/> : null}
+            <span css={{marginLeft:5}}>{modalMessage}</span>
+            {sourceDownloadStatus === "pending" ||
             sourceDownloadStatus === null ? (
               <Spinner />
             ) : null}
@@ -77,5 +97,5 @@ export default function DownloadModal({ modalState, setModalState }) {
         </DialogContent>
       </Dialog>
     </div>
-    : null
+  ) : null;
 }
