410ad2316959c882c7c351ac82fc4fb17e3827b8
bfox6 <bfox6@nd.edu>
2020-01-29 15:45:14 -0500

    New Upload Modal Component at highest level

---
+++ src/components/UploadModal.js
@@ -0,0 +1,35 @@
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import React from "react";
+import {useDispatch} from "react-redux";
+import UploadStepper from "./widgets/UploadStepper/UploadStepper";
+import DialogTitle from "./modals/modalHeader";
+
+/**
+ * This component is responsible for displaying the upload modal content including the stepper.
+ **/
+export default function UploadModal({ modalState, setModalState, selectedInSource }) {
+  const dispatch = useDispatch();
+
+  /**
+   * When the 'x' is pressed on the modal clear the upload data, remove the upload error
+   * from APIOperationErrors if it exists, and toggle the modal.
+   **/
+  const handleClose = () => {
+    setModalState(false);
+    dispatch(actionCreators.resources.clearUploadData());
+    dispatch(actionCreators.resources.removeFromErrorList(actionCreators.resources.uploadToSourceTarget.toString()))};
+
+  return modalState
+    ? <div>
+      <Dialog maxWidth="md" fullWidth={true} open={modalState} onClose={handleClose} aria-labelledby={"form-dialog-title"}>
+        <DialogTitle id="form-dialog-title" onClose={handleClose}>
+          Upload Resource
+        </DialogTitle>
+        <DialogContent>
+          <UploadStepper selectedInSource={selectedInSource}/>
+        </DialogContent>
+      </Dialog>
+    </div>
+    : null
+}
