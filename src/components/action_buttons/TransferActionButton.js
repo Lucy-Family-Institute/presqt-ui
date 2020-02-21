c6c9e36b77c4ece9b2a1ac3c039842b1baa15cfa
Justin Branco <jbranco@nd.edu>
2020-02-21 13:51:17 -0500

    BRETT, HELP!

--- src/components/action_buttons/TransferActionButton.js
+++ src/components/action_buttons/TransferActionButton.js
@@ -3,15 +3,17 @@
 import { jsx } from "@emotion/core";
 import textStyles from "../../styles/text";
 import ActionButton from "../widgets/buttons/ActionButton";
+import SplitButton from "../widgets/buttons/SplitButton";
 
 export default function TransferActionButton({ disabled }) {
+  const options = ['Transfer to Existing', 'Transfer New Resource'];
+  
   return (
-    <ActionButton
-      elevation={0}
-      variant="contained"
-      disabled={disabled}
-    >
-      <span css={textStyles.buttonText}>Transfer</span>
-    </ActionButton>
+    <SplitButton
+      options={options}
+      // elevation={0}
+      // variant="contained"
+      // disabled={disabled}
+    />
   )
 }
