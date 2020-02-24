1e81c4a90e607654887839797f7fcd82bb93ae79
Justin Branco <jbranco@nd.edu>
2020-02-24 14:57:59 -0500

    Display selected resource in stepper

--- src/components/transfer_stepper/TransferStepperSelectResource.js
+++ src/components/transfer_stepper/TransferStepperSelectResource.js
@@ -1,4 +1,17 @@
-export default function TransferStepperSelectResource() {
-  return 'hi'
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { useSelector } from "react-redux";
+import text from '../../styles/text';
 
+export default function TransferStepperSelectResource() {
+  const selectedTransferResource = useSelector(state => state.resources.selectedTransferResource);
+
+  if (selectedTransferResource) {
+    return (
+      <div css={text.body}>Selected Resource: <span css={text.selectedResource}>{selectedTransferResource.title}</span></div>
+      )
+  }
+  return (
+    <div css={text.noSelectedResource}>No Resource Selected</div>
+  )
 }
