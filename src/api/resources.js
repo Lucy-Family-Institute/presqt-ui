55dfe171759b3c2f52e3e34b09384d43eeaeed1b
bfox6 <bfox6@nd.edu>
2020-02-25 15:48:12 -0500

    Remove the poop

--- src/api/resources.js
+++ src/api/resources.js
@@ -6,7 +6,7 @@ import { apiURLBase } from '../config';
  **/
 export function getTargetResources(target, targetToken) {
   if (target === 'osf') {
-    return axios.get(`${apiURLBase}targets/poop/resources/`, {
+    return axios.get(`${apiURLBase}targets/${target}/resources/`, {
       headers: {'presqt-source-token': targetToken}
     });
   }
