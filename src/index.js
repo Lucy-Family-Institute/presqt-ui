f9f509eb219aa4f331aca779e74324ca98977200
bfox6 <bfox6@nd.edu>
2020-02-28 16:35:06 -0500

    Removed combineReducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import mainReducers from './redux/reducers/main';
@@ -9,12 +9,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootSaga from './redux/saga/saga_root';
-
-// const rootReducer = combineReducers({
-//   authorization: authorizationReducer,
-//   targets: targetsReducer,
-//   resources: resourcesReducer
-// });
 
 const sagaMiddleware = createSagaMiddleware();
 
