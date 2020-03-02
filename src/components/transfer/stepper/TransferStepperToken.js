c82930bf48454224e792fc565a4fc3d8befaa804
Justin Branco <jbranco@nd.edu>
2020-03-02 16:27:35 -0500

    Fixed

--- src/components/transfer/stepper/TransferStepperToken.js
+++ src/components/transfer/stepper/TransferStepperToken.js
@@ -3,7 +3,7 @@ import {jsx} from "@emotion/core";
 import {actionCreators} from "../../../redux/actionCreators";
 import SearchTextField from "../../widgets/text_fields/SearchTextField";
 import { withStyles } from "@material-ui/styles";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 const TokenTextField = withStyles({
   root: {
@@ -13,6 +13,7 @@ const TokenTextField = withStyles({
 
 export default function TransferStepperToken({ handleNext }) {
   const dispatch = useDispatch();
+  const transferDestinationToken = useSelector(state => state.transferDestinationToken)
 
   return (
     <TokenTextField
@@ -21,7 +22,7 @@ export default function TransferStepperToken({ handleNext }) {
       label="Insert API Token Here"
       onChange={event => dispatch(actionCreators.transfer.saveTransferToken(event.target.value))}
       // If the enter button is pressed (code 13), go to the next step.
-      onKeyDown={(event) => {if (event.keyCode === 13 && destinationToken !== '') {handleNext()}}}
+      onKeyDown={(event) => {if (event.keyCode === 13 && transferDestinationToken !== '') {handleNext()}}}
     />
   )
 }
