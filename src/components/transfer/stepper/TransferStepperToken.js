67ef8a41482f19e7fa894effb7dde4cb366e0115
Justin Branco <jbranco@nd.edu>
2020-03-03 10:24:23 -0500

    Works for transfer as well

--- src/components/transfer/stepper/TransferStepperToken.js
+++ src/components/transfer/stepper/TransferStepperToken.js
@@ -3,7 +3,8 @@ import {jsx} from "@emotion/core";
 import {actionCreators} from "../../../redux/actionCreators";
 import SearchTextField from "../../widgets/text_fields/SearchTextField";
 import { withStyles } from "@material-ui/styles";
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {testTokens, testTokenCommand} from "../../../config";
 
 const TokenTextField = withStyles({
   root: {
@@ -14,13 +15,26 @@ const TokenTextField = withStyles({
 export default function TransferStepperToken({ handleNext }) {
   const dispatch = useDispatch();
   const transferDestinationToken = useSelector(state => state.transferDestinationToken)
+  const transferDestinationTarget = useSelector(state => state.transferDestinationTarget);
+
+  const tokenSubmit = (value) => {
+    let finalValue = value;
+    if (value === testTokenCommand) {
+      for (var key in testTokens) {
+        if (key === transferDestinationTarget) {
+          finalValue = testTokens[key]
+        }
+      }
+    };
+    dispatch(actionCreators.transfer.saveTransferToken(finalValue))
+  };
 
   return (
     <TokenTextField
       size="small"
       type='text'
       label="Insert API Token Here"
-      onChange={event => dispatch(actionCreators.transfer.saveTransferToken(event.target.value))}
+      onChange={event => tokenSubmit(event.target.value)}
       // If the enter button is pressed (code 13), go to the next step.
       onKeyDown={(event) => {if (event.keyCode === 13 && transferDestinationToken !== '') {handleNext()}}}
     />
