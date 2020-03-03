19bbcad8c7d6bdbeeb33d9a86b1859d54d679c99
Justin Branco <jbranco@nd.edu>
2020-03-03 13:44:46 -0500

    Back to the beginning

--- src/components/transfer/stepper/TransferStepperSelectResource.js
+++ src/components/transfer/stepper/TransferStepperSelectResource.js
@@ -1,21 +1,12 @@
 /** @jsx jsx */
 import { jsx, css } from "@emotion/core";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import text from '../../../styles/text';
-import { actionCreators } from "../../../redux/actionCreators";
+import { useEffect } from 'react';
+import { actionCreators } from '../../../redux/actionCreators';
 
 export default function TransferStepperSelectResource({ setActiveStep }) {
-  const dispatch = useDispatch();
-
   const selectedTransferResourceName = useSelector(state => state.selectedTransferResourceName);
-  const apiOperationErrors = useSelector(state => state.apiOperationErrors);
-
-  if (apiOperationErrors) {
-    dispatch(actionCreators.resources.removeFromErrorList(
-      actionCreators.transfer.loadFromTransferTarget.toSring()
-    ));
-    setActiveStep(1);
-  }
 
   if (selectedTransferResourceName) {
     return (
