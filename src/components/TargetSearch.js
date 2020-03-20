cf765df588a35ce5dfaee8002574f6343562a372
bfox6 <bfox6@nd.edu>
2020-03-20 13:32:56 -0400

    bfox6__search_details

--- src/components/TargetSearch.js
+++ src/components/TargetSearch.js
@@ -8,6 +8,7 @@ import SearchTextField from "./widgets/text_fields/SearchTextField";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
 import InfoIcon from '@material-ui/icons/Info';
+import Grid from "@material-ui/core/Grid";
 
 
 const useStyles = makeStyles({
@@ -46,30 +47,45 @@ export default function TargetSearch() {
   };
 
   return (
-    <Fragment>
-    <div css={{ marginBottom: 10 }}>
-      <form
-        onSubmit={event => submitSearch(event)}
-        className={classes.root}
-        noValidate
-        autoComplete="off"
+    <Grid container spacing={1} alignItems="flex-end">
+      <Grid item>
+        <div css={{ marginBottom: 10 }}>
+          <form
+            onSubmit={event => submitSearch(event)}
+            className={classes.root}
+            noValidate
+            autoComplete="off"
+          >
+            <SearchTextField
+              size="small"
+              type="text"
+              id="outlined-basic"
+              label={`Search ${selectedTarget.readable_name} By Title`}
+              variant="outlined"
+              value={searchValue}
+              onChange={event => setSearchValue(event.target.value)}
+            />
+          </form>
+        </div>
+      </Grid>
+      <Grid
+        style={{marginBottom: 14}}
+        item
       >
-        <SearchTextField
-          size="small"
-          type="text"
-          id="outlined-basic"
-          label={`Search ${selectedTarget.readable_name} By Title`}
-          variant="outlined"
-          value={searchValue}
-          onChange={event => setSearchValue(event.target.value)}
-        />
-        <Tooltip title="Stuff" arrow placement="right">
-          <IconButton aria-label="info">
-            <InfoIcon />
-          </IconButton>
-        </Tooltip>
-      </form>
-    </div>
-    </Fragment>
+          <Tooltip
+            title={
+              <Fragment>
+                Only the first page of paginated search results are returned.
+                <br />
+                If you don't see the desired project try a more specific search.
+              </Fragment>
+            }
+            arrow placement="right">
+            <InfoIcon
+              style={{ color: '#757575' }}
+            />
+          </Tooltip>
+      </Grid>
+    </Grid>
   );
 }
