e49ba8680448db649489421174c866d7da909df7
bfox6 <bfox6@nd.edu>
2020-03-25 10:39:30 -0400

    Added button

--- src/components/modals/EaasiModal.js
+++ src/components/modals/EaasiModal.js
@@ -6,6 +6,19 @@ import {jsx} from "@emotion/core";
 import {useDispatch, useSelector} from "react-redux";
 import React from "react";
 import {actionCreators} from "../../redux/actionCreators";
+import textStyles from "../../styles/text";
+import Button from "@material-ui/core/Button";
+import withStyles from "@material-ui/core/styles/withStyles";
+import colors from "../../styles/colors";
+
+const CustomEaasiButton = withStyles({
+  root: {
+    backgroundColor: colors.presqtBlue,
+    '&:hover': {
+      backgroundColor: '#0a4996',
+    },
+  },
+})(Button);
 
 export default function EaasiModal() {
   const dispatch = useDispatch();
@@ -38,7 +51,20 @@ export default function EaasiModal() {
           EaaSI Time, Baby!
         </DialogTitle>
         <DialogContent style={{ padding: 20 }}>
-          Eaasi Stuff!
+            <div css={{ display: 'flex',  justifyContent:'center' }}>
+              <p>
+                Yo, click this button and Eaasi gets yo stuff and proposes some shiz
+              </p>
+            </div>
+            <div css={{ display: 'flex',  justifyContent:'center' }}>
+              <CustomEaasiButton
+                // onClick={submitUpload}
+                variant="contained"
+                color="primary"
+              >
+                <span css={textStyles.buttonText}>Send</span>
+              </CustomEaasiButton>
+            </div>
         </DialogContent>
       </Dialog>
     </div>
