d1bbd26609947705ca8ad96574f2431fce80d3fb
Justin Branco <jbranco@nd.edu>
2020-03-26 15:08:56 -0400

    SAGA

--- src/redux/saga/eaasi.js
+++ src/redux/saga/eaasi.js
@@ -1,6 +1,6 @@
-import {call, put, takeEvery} from "@redux-saga/core/effects";
+import {call, put, delay, takeEvery} from "@redux-saga/core/effects";
 import {actionCreators} from "../actionCreators";
-import {postEaasiProposal} from "../../api/eaasi";
+import {postEaasiProposal, getEaasiProposal} from "../../api/eaasi";
 
 /** EaaSI Proposal **/
 export function* watchEaasiProposal() {
@@ -16,10 +16,40 @@ function* sendEaasiProposal(action) {
     yield put(actionCreators.eaasi.sendEaasiProposalSuccess(response.data))
   }
   catch (error) {
-    console.log(error.response.data);
     yield put(actionCreators.eaasi.sendEaasiProposalFailure(
       error.response.status,
       error.response.data.error)
     )
+  }
+}
+
+/** Get EaaSI Stuff **/
+export function* watchEaasiSuccess() {
+  yield takeEvery(actionCreators.eaasi.getEaasiProposal, getEaasiProposalLink);
+
+  function* getEaasiProposalLink(action) {
+    try {
+      let proposalFinished = false
+      while (!proposalFinished) {
+        const response = yield call(
+          getEaasiProposal,
+          action.payload.proposal_link
+        )
+        if (response.status === 200) {
+          yield put(actionCreators.eaasi.getEaasiProposalSuccess(response.data));
+          proposalFinished = true;
+        }
+        else {
+          yield put(actionCreators.eaasi.getEaasiProposalSuccess(response.data));
+          yield delay(5000);
+        }
+      }
+    }
+    catch (error) {
+      yield put(actionCreators.eaasi.getEaasiProposalFailure(
+        error.response.status,
+        error.response.data.error)
+      )
+    }
   }
 }
