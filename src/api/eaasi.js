552af435acc8b0821aff39b90a3d0231604cfdec
Justin Branco <jbranco@nd.edu>
2020-03-26 15:08:25 -0400

    get EaaSI call

--- src/api/eaasi.js
+++ src/api/eaasi.js
@@ -4,9 +4,12 @@ import axios from "axios";
 /** EaaSI Proposal **/
 export function postEaasiProposal(ticket_number) {
   const proposalURL = `${apiURLBase}services/eaasi/proposals/`;
-  console.log(proposalURL, ticket_number);
   const bodyFormData = new FormData();
   bodyFormData.set('ticket_number', ticket_number);
 
   return axios.post(proposalURL, bodyFormData);
+}
+
+export function getEaasiProposal(proposal_link) {
+  return axios.get(proposal_link);
 }
