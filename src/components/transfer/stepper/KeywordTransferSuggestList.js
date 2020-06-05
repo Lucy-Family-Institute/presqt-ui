9e0f723d5a007acd2a83761debb09a2e2a8b9279
Justin Branco <jbranco@nd.edu>
2020-06-05 14:50:02 -0400

    Add your own keywords~

--- src/components/transfer/stepper/KeywordTransferSuggestList.js
+++ src/components/transfer/stepper/KeywordTransferSuggestList.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
@@ -8,8 +8,9 @@ import {useSelector} from "react-redux";
 import Checkbox from "@material-ui/core/Checkbox";
 import colors from "../../../styles/colors";
 import {makeStyles} from "@material-ui/core/styles";
-import WarningList from "../../widgets/list_items/WarningList";
-import Grid from "@material-ui/core/Grid";
+import SearchTextField from "../../widgets/text_fields/SearchTextField";
+import { withStyles } from "@material-ui/styles";
+import isSpaces from "../../../utils/isSpaces";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,11 +24,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const KeywordTextField = withStyles({
+  root: {
+    width: '75%'
+  }
+})(SearchTextField);
+
 export default function KeywordTransferSuggestList({setKeywordList, keywordList}) {
   const classes = useStyles();
   const keywords = useSelector(state => state.keywords);
-  const selectedResource = useSelector(state => state.selectedResource);
   const [checked, setChecked] = useState([]);
+  const [newKeyValue, setNewKeyValue] = useState("");
+  const [newKeywordList, setNewKeywordList] = useState([])
+  
+  useEffect(() => {
+    if (keywords) {
+      setNewKeywordList([...keywords.enhanced_keywords]);
+    }
+  }, [])
+  
 
   const handleToggle = (value) => {
     const currentIndex = checked.indexOf(value);
@@ -44,49 +59,63 @@ export default function KeywordTransferSuggestList({setKeywordList, keywordList}
     setKeywordList(newChecked);
   };
 
-  if (!keywords) {
-    return (
-      <Grid item md={12}>
-        <WarningList resources={[`No keywords or enhancements found for resource "${selectedResource.title}".`]} header=""/>
-      </Grid>
-    )
+  const searchKeystroke = (event) => {
+    setNewKeyValue(event.target.value);
   }
-  else {
-    return (
-      <List
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader" disableSticky={true}>
-            Select the following keyword suggestions to enhance:
-        </ListSubheader>
-        }
-      >
-        {keywords.enhanced_keywords.map((value, index) => {
-          const labelId = `checkbox-list-label-${value}`;
-          return (
-            <ListItem
-              key={value + index}
-              role={undefined}
-              dense
-              button
-              onClick={() => handleToggle(value)}
-            >
-              <ListItemIcon>
-                <Checkbox
-                  style={{ color: colors.presqtBlue }}
-                  edge="start"
-                  checked={checked.indexOf(value) !== -1}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                  classes={{ root: classes.checked }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={`${value}`} />
-            </ListItem>
 
-          )
-        })}
-      </List>
-    )
+  const putInList = () => {
+    const newChecked = [...checked];
+    // Add the new keyword to the list
+    newKeywordList.push(newKeyValue);
+    newChecked.push(newKeyValue)
+    setChecked(newChecked);
+    setKeywordList(newChecked);
+    setNewKeyValue('');
   }
+
+  return (
+    <List
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader" disableSticky={true}>
+          {!keywords
+            ? "No keywords found for this resource."
+            : "Select the following keyword suggestions to enhance:"
+            }
+      </ListSubheader>
+      }
+    >
+      {newKeywordList.map((value, index) => {
+        const labelId = `checkbox-list-label-${value}`;
+        return (
+          <ListItem
+            key={value + index}
+            role={undefined}
+            dense
+            button
+            onClick={() => handleToggle(value)}
+          >
+            <ListItemIcon>
+              <Checkbox
+                style={{ color: colors.presqtBlue }}
+                edge="start"
+                checked={checked.indexOf(value) !== -1}
+                tabIndex={-1}
+                disableRipple
+                inputProps={{ "aria-labelledby": labelId }}
+                classes={{ root: classes.checked }}
+              />
+            </ListItemIcon>
+            <ListItemText id={labelId} primary={`${value}`} />
+          </ListItem>
+        )
+      })}
+          <KeywordTextField
+            size="small"
+            label="Add Your Own Keywords Here"
+            value={newKeyValue}
+            onKeyDown={(event) => { event.keyCode === 13 && !isSpaces(event.target.value) ? putInList() : null }}
+            onChange={event => searchKeystroke(event)}
+            />
+    </List>
+  )
 }
