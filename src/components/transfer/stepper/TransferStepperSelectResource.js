af1cc9d2fb58f7845d60047af15a7b01d17513ba
Brett Fox <brettmfox33@gmail.com>
2020-07-28 10:56:04 -0400

    Made things less disambiguous for transfer stepper

--- src/components/transfer/stepper/TransferStepperSelectResource.js
+++ src/components/transfer/stepper/TransferStepperSelectResource.js
@@ -9,22 +9,13 @@ import TransferCancelSelectionButton from "../TransferCancelSelectionButton"
 export default function TransferStepperSelectResource() {
   const selectedTransferResourceName = useSelector(state => state.selectedTransferResourceName);
 
-  if (selectedTransferResourceName) {
     return (
-      <div css={text.transferResource}>Selected Resource: <span css={text.selectedResource}>{selectedTransferResourceName}
-        <TransferCancelSelectionButton
-          disabled={false}
-        />
-      </span>
+      <div css={text.transferResource}>Selected Resource:
+        <span css={!selectedTransferResourceName ? text.noSelectedResource : text.selectedResource}>{!selectedTransferResourceName ? ' None' : ` ${selectedTransferResourceName}`}
+          <TransferCancelSelectionButton
+            disabled={!selectedTransferResourceName}
+          />
+        </span>
       </div>
     )
-  }
-  return (
-    <div css={text.transferResource}>
-        No Resource Selected
-        <span>
-          <TransferCancelSelectionButton disabled={true} />
-        </span>
-    </div>
-  )
 }
