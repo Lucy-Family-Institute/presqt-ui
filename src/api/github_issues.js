bfc9cac8373ccec76aae6cec19cae0037ec82ed4
Justin Branco <jbranco@nd.edu>
2020-08-18 14:30:00 -0400

    Added comment

--- src/api/github_issues.js
+++ src/api/github_issues.js
@@ -3,6 +3,7 @@ import { githubToken } from "../config";
 
 export function postGithubIssue(title, body) {
   const issueUrl = "https://api.github.com/repos/ndlib/presqt-ui/issues";
+  // Replace newlines with newline character
   const newBody = body.replace(/(?:\r\n|\r|\n)/g, '\\n')
   const bodyJSON = `{"title": "${title}", "body": "${newBody}"}`;
   
