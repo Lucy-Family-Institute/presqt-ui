8a36ed3d38f987a4f2dbb894c6edd09d4bc19712
Justin Branco <jbranco@nd.edu>
2020-08-31 16:02:35 -0400

    Messages on enhancement

--- src/components/keyword_stepper/KeywordStepperResults.js
+++ src/components/keyword_stepper/KeywordStepperResults.js
@@ -1,6 +1,6 @@
 import Spinner from "../widgets/spinners/Spinner";
 import useDefault from "../../hooks/useDefault";
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, Component } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import KeywordList from "../widgets/list_items/KeywordsList";
 import colors from "../../styles/colors";
@@ -24,7 +24,11 @@ const CustomKeywordButton = withStyles({
 
 export default function KeywordStepperResults({ newKeywords }) {
   const dispatch = useDispatch();
-  
+  // These 3 variables are for the message displayed while enhancing keywords
+  const messageOptions = ["Updating keywords at the source...", "Updating PRESQT_FTS_METADATA file...", "Finalizing changes..."];
+  const [message, setMessage] = useDefault(messageOptions[0]);
+  let index = 0;
+
   const apiOperationErrors = useSelector((state) => state.apiOperationErrors);
   const selectedResource = useSelector((state) => state.selectedResource);
   const updatedKeywords = useSelector((state) => state.updatedKeywords);
@@ -34,11 +38,31 @@ export default function KeywordStepperResults({ newKeywords }) {
     state.selectedTarget ? state.apiTokens[state.selectedTarget.name] : null
   );
   // This resource is needed to refresh the detail
-  const targetResources = useSelector(state => state.targetResources);
-  const resource = targetResources.find(resource => resource.id === selectedResource.id);
+  const targetResources = useSelector((state) => state.targetResources);
+  const resource = targetResources.find(
+    (resource) => resource.id === selectedResource.id
+  );
+
+  // This useEffect is triggered only when the KeywordStepperResults component is mounted.
+  // when using setInterval, it is imperative that you clear the scheduled interval once the component unmounts.
+  // Thus the clearInterval..
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (index < 3) {
+        setMessage(messageOptions[index]);
+        index += 1;
+      } else {
+        // Nothing happens here.
+      }
+    }, 3500);
+    return () => clearInterval(interval);
+  }, []);
 
   const [stepContent, setStepContent] = useDefault(
     <div>
+      <div css={{paddingBottom: 15, display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "row"}}>
+        {message}
+      </div>
       <Spinner />
     </div>
   );
@@ -62,72 +86,94 @@ export default function KeywordStepperResults({ newKeywords }) {
   };
 
   useEffect(() => {
-    if (keywordStatus === "postSuccess") {
-      setStepContent(
-        <Fragment>
-          <List dense={true}>
-            <SuccessListItem message={"Keywords Enhanced Successfully."}/>
-          </List>
-          <KeywordList
-            resources={updatedKeywords.keywords_added.sort(function(stringA, stringB) {return stringA.localeCompare(stringB)})}
-            header={`The following keywords were added to ${selectedResource.title}:`}
-            colNumber={2}
-          />
-          <KeywordList
-            resources={updatedKeywords.final_keywords.sort(function(stringA, stringB) {return stringA.localeCompare(stringB)})}
-            header={`The following are all keywords for ${selectedResource.title}:`}
-            colNumber={2}
-          />
-        </Fragment>
-      );
-      dispatch(actionCreators.resources.selectResource(resource, targetToken));
-    }
-    else if (keywordStatus === "postFailure") {
-      setStepContent(
-        <Fragment>
-          <div
-            style={{ display: "flex", justifyContent: "center", padding: 10 }}
-          >
+    if (keywordStatus) {
+      if (keywordStatus === "postSuccess") {
+        setStepContent(
+          <Fragment>
+            <List dense={true}>
+              <SuccessListItem message={"Keywords Enhanced Successfully."} />
+            </List>
+            <KeywordList
+              resources={updatedKeywords.keywords_added.sort(function (
+                stringA,
+                stringB
+              ) {
+                return stringA.localeCompare(stringB);
+              })}
+              header={`The following keywords were added to ${selectedResource.title}:`}
+              colNumber={2}
+            />
+            <KeywordList
+              resources={updatedKeywords.final_keywords.sort(function (
+                stringA,
+                stringB
+              ) {
+                return stringA.localeCompare(stringB);
+              })}
+              header={`The following are all keywords for ${selectedResource.title}:`}
+              colNumber={2}
+            />
+          </Fragment>
+        );
+        dispatch(
+          actionCreators.resources.selectResource(resource, targetToken)
+        );
+      } else if (keywordStatus === "postFailure") {
+        setStepContent(
+          <Fragment>
             <div
-              style={{
-                paddingTop: 20,
-                paddingBottom: 20,
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
+              style={{ display: "flex", justifyContent: "center", padding: 10 }}
             >
-              <ErrorOutlineIcon color="error" />
-              <span style={{ marginLeft: 5, color: colors.chevelleRed }}>
-                {keywordPostError.data.error}
-              </span>
-            </div>
-          </div>
-          <div
-            style={{ display: "flex", justifyContent: "center", padding: 5 }}
-          >
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <CustomKeywordButton
-                onClick={retryKeywords}
-                variant="contained"
-                color="primary"
+              <div
+                style={{
+                  paddingTop: 20,
+                  paddingBottom: 20,
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
               >
-                <span css={textStyles.buttonText}>Retry</span>
-              </CustomKeywordButton>
+                <ErrorOutlineIcon color="error" />
+                <span style={{ marginLeft: 5, color: colors.chevelleRed }}>
+                  {keywordPostError.data.error}
+                </span>
+              </div>
             </div>
+            <div
+              style={{ display: "flex", justifyContent: "center", padding: 5 }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <CustomKeywordButton
+                  onClick={retryKeywords}
+                  variant="contained"
+                  color="primary"
+                >
+                  <span css={textStyles.buttonText}>Retry</span>
+                </CustomKeywordButton>
+              </div>
+            </div>
+          </Fragment>
+        );
+      } else {
+        setStepContent(
+          <div>
+            <div style={{paddingBottom: 15, display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "row"}}>
+              {message}
+            </div>
+            <Spinner />
           </div>
-        </Fragment>
-      );
+        );
+      }
     }
-  }, [keywordStatus]);
+  }, [keywordStatus, message]);
 
   return stepContent;
 }
