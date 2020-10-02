5fd8f7156ef7a546d961f04387a31a6d407e772d
Justin Branco <jbranco@nd.edu>
2020-10-02 12:48:26 -0400

    Add tests to POST request

--- src/api/fairshare.js
+++ src/api/fairshare.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { apiURLBase } from "../config";
 
-export function postFairshareEvaluator(doi) {
+export function postFairshareEvaluator(doi, testList) {
   return axios.post(`${apiURLBase}services/fairshare/evaluator/`, {
-    resource_id: doi,
+    "resource_id": doi,
+    "tests": testList,
   });
 }
 
 export function getFairshareEvaluatorTests() {
-  console.log("HERE");
   return axios.get(`${apiURLBase}services/fairshare/evaluator/`);
 }
--- src/components/fairshare_stepper/FAIRshareStepperSubmit.js
+++ src/components/fairshare_stepper/FAIRshareStepperSubmit.js
@@ -1,12 +1,13 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import colors from "../../styles/colors";
 import Button from "@material-ui/core/Button/Button";
 import textStyles from "../../styles/text";
 import { useDispatch, useSelector } from "react-redux";
 import { actionCreators } from "../../redux/actionCreators";
+import FAIRshareTestsList from "../widgets/list_items/FAIRshareTestsList";
 
 const CustomFairshareButton = withStyles({
   root: {
@@ -21,19 +22,32 @@ export default function FAIRshareStepperSubmit({ setActiveStep }) {
   const dispatch = useDispatch();
 
   const selectedResource = useSelector((state) => state.selectedResource);
+  const fairshareTests = useSelector((state) => state.fairshareTests);
+  const [newTests, setNewTests] = useState([]);
 
   const submitEvaluation = () => {
-    dispatch(actionCreators.fairshare.sendFairshareEvaluation(selectedResource.doi));
+    // We need to build a list of all the test IDs
+    let testsToPost = []
+    const pushThoseTests = (newTests.map((value, index) => {
+      testsToPost.push(value.test_id);
+    }));
+    dispatch(actionCreators.fairshare.sendFairshareEvaluation(selectedResource.doi, testsToPost));
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
   return (
     <Fragment>
       <div css={{ paddingBottom: 10 }}>
-        Submit a FAIRshare Evaluator request for <i>{selectedResource.title}</i> with
-        doi <i>{selectedResource.doi}</i>. They will return the results of a number
-        of tests identified as of interest to the PresQT community.
+        Submit a FAIRshare Evaluator request for <i>{selectedResource.title}</i>{" "}
+        with doi <i>{selectedResource.doi}</i>. They will return the results of
+        a number of tests identified as of interest to the PresQT community.
       </div>
+      <FAIRshareTestsList
+        tests={fairshareTests}
+        header={"List of FAIRshare tests supported by PresQT"}
+        setNewTests={setNewTests}
+        newTests={newTests}
+      />
       <CustomFairshareButton
         onClick={submitEvaluation}
         variant="contained"
