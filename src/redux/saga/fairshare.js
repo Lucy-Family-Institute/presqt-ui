48a84172128917189f7831329157c25ace570596
Justin Branco <jbranco@nd.edu>
2020-10-02 12:49:26 -0400

    New saga call to get all FAIRshare tests supported

--- src/redux/saga/fairshare.js
+++ src/redux/saga/fairshare.js
@@ -1,22 +1,30 @@
 import { call, put, delay, takeEvery } from "@redux-saga/core/effects";
 import { actionCreators } from "../actionCreators";
-import { postFairshareEvaluator, getFairshareEvaluatorTests } from "../../api/fairshare";
+import {
+  postFairshareEvaluator,
+  getFairshareEvaluatorTests,
+} from "../../api/fairshare";
 
 /** FAIRshare Evaluation POST **/
 export function* watchFairshareEvaluation() {
-  yield takeEvery(actionCreators.fairshare.sendFairshareEvaluation, sendFairshareEvaluation);
+  yield takeEvery(
+    actionCreators.fairshare.sendFairshareEvaluation,
+    sendFairshareEvaluation
+  );
 }
 
 function* sendFairshareEvaluation(action) {
   try {
     const response = yield call(
-        postFairshareEvaluator,
-        action.payload.doi
+      postFairshareEvaluator,
+      action.payload.doi,
+      action.payload.testList
     );
 
-    yield put(actionCreators.fairshare.sendFairshareEvaluationSuccess(response.data));
-  }
-  catch (error) {
+    yield put(
+      actionCreators.fairshare.sendFairshareEvaluationSuccess(response.data)
+    );
+  } catch (error) {
     console.log(error);
     yield put(
       actionCreators.fairshare.sendFairshareEvaluationFailure(
@@ -27,14 +35,15 @@ function* sendFairshareEvaluation(action) {
   }
 }
 
-
 /** Get FAIRshare tests list **/
 export function* watchGetFairshareEvaluationTests() {
-  yield takeEvery(actionCreators.fairshare.loadFairshareTests, loadFairshareTests);
+  yield takeEvery(
+    actionCreators.fairshare.loadFairshareTests,
+    loadFairshareTests
+  );
 }
 
 function* loadFairshareTests() {
-    const response = yield call(getFairshareEvaluatorTests);
-    console.log(response.data);
-    yield put(actionCreators.fairshare.loadFairshareTestsSuccess(response.data));
+  const response = yield call(getFairshareEvaluatorTests);
+  yield put(actionCreators.fairshare.loadFairshareTestsSuccess(response.data));
 }
