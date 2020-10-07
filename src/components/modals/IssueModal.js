b0b3dcd7f8dd36b333369c942f513fbe9910e009
Justin Branco <jbranco@nd.edu>
2020-10-07 15:50:34 -0400

    Filter profanity

--- src/components/modals/IssueModal.js
+++ src/components/modals/IssueModal.js
@@ -14,6 +14,8 @@ import useDefault from "../../hooks/useDefault";
 
 export default function IssueModal() {
   const dispatch = useDispatch();
+  var Filter = require('bad-words');
+  const filter = new Filter();
 
   const issueModalDisplay = useSelector(state => state.issueModalDisplay);
   const githubStatus = useSelector(state => state.githubStatus);
@@ -62,7 +64,7 @@ export default function IssueModal() {
               <TokenTextField
                 id="outlined-multiline-static"
                 label="Insert Content of The Issue Here..."
-                onChange={event => setBody(event.target.value)}
+                onChange={event => setBody(filter.clean(event.target.value))}
                 value={body}
                 multiline
                 rows="4"
