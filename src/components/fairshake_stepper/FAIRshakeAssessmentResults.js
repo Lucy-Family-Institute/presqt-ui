062a661b6c6446df2c663ba220d1afde8df7973d
Justin Branco <jbranco@nd.edu>
2020-12-03 10:02:44 -0500

    Initial results page

---
+++ src/components/fairshake_stepper/FAIRshakeAssessmentResults.js
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import {Fragment, useEffect, useState} from "react";
+import Spinner from "../widgets/spinners/Spinner";
+import {actionCreators} from "../../redux/actionCreators";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
+import {jsx} from "@emotion/core";
+import {useDispatch, useSelector} from "react-redux";
+import colors from "../../styles/colors";
+import CheckCircleOutlineIcon from "@material-ui/icons/CheckCircleOutline";
+import getError from "../../utils/getError";
+
+
+export default function FAIRshakeAssessmentResults() {
+  const dispatch = useDispatch();
+
+  const [modalContentHeader, setModalContentHeader] = useState("");
+  const [modalContentBody, setModalContentBody] = useState("");
+
+  const fairshakeAssessmentResults = useSelector(state => state.fairshakeAssessmentResults)
+  const fairshakeAssessmentStatus = useSelector(state => state.fairshakeAssessmentStatus)
+  const fairshakeAssessmentError = getError(actionCreators.fairshake.submitFairshakeAssessment)
+
+  useEffect(() => {
+    if (fairshakeAssessmentStatus === 'postPending') {
+        setModalContentHeader("Sending assessment details to FAIRshake...");
+        setModalContentBody(<Spinner />);
+    }
+    else if (fairshakeAssessmentStatus === 'postFailure') {
+        setModalContentHeader(
+            <div
+            css={{ paddingTop: 20, paddingBottom: 20, display: 'flex', flexDirection: 'row', alignItems: 'center',  justifyContent: 'center' }}>
+            <ErrorOutlineIcon color="error" />
+            <span css={{ marginLeft: 5 }}>FAIRshake Error: {fairshakeAssessmentError.data.message}</span>
+            </div>);
+    setModalContentBody("");
+    }
+    else if (fairshakeAssessmentStatus === 'postFinished') {
+        setModalContentHeader(
+            <Fragment>
+              <CheckCircleOutlineIcon
+                style={{ color: colors.successGreen, paddingRight:5 }}
+              />
+              FAIRshake has successfully registered a digital object and created your assessment. It can be found&nbsp;<a href={`https://fairshake.cloud/digital_object/${fairshakeAssessmentResults.digital_object_id}/assessments/`} target="_blank">here.</a>
+            </Fragment>
+          );
+          setModalContentBody("");
+    }
+    
+  }, [fairshakeAssessmentResults, fairshakeAssessmentError, fairshakeAssessmentStatus])
+
+  return(
+    <Fragment>
+      <div css={{ display: "flex", justifyContent: "center", padding: 10 }}>
+        {modalContentHeader}
+      </div>
+      <div css={{ display: "flex", justifyContent: "center" }}>
+        {modalContentBody}
+      </div>
+    </Fragment>
+  )
+}
